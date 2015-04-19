e95db909908366b50cdd162209cedafc006a64f4
Rick Cid <rick@hispand.com>
2015-04-18 23:57:32 -0600

    Added Grid filtering option

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,11 +1,35 @@
-var app = angular.module('itunes');
+var app = angular.module('itunes', ['ngGrid']);
 
 app.controller('mainCtrl', function($scope, itunesService){
   //This is setting up the default behavior of our ng-grid. The important thing to note is
   //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
   //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
-  $scope.gridOptions = { 
-  //$scope.myData = {
+  $scope.options = [{raw: "trackCensoredName", pretty: "Song"}, {raw: "collectionName", pretty: "Album"}, {raw: "kind", pretty: "Movie"}];
+
+  $scope.filterOptions = {
+    filterText: ''
+  };
+
+  $scope.gridOptions = {
+    data: 'songData',
+    height: '110px',
+    sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
+    columnDefs: [
+        {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
+        {field: 'Song', displayName: 'Song'},
+        {field: 'Artist', displayName: 'Artist'},
+        {field: 'Collection', displayName: 'Collection'},
+        {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
+        {field: 'Type', displayName: 'Type'},
+        {field: 'TrackPrice', displayName: 'Single Price'},
+        {field: 'CollectionPrice', displayName: 'Collection Price'},
+    ],
+    filterOptions: $scope.filterOptions
+  };
+
+
+
+  /*$scope.gridOptions = { 
       data: 'songData',
       height: '110px',
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
@@ -19,7 +43,7 @@ app.controller('mainCtrl', function($scope, itunesService){
         {field: 'TrackPrice', displayName: 'Single Price'},
         {field: 'CollectionPrice', displayName: 'Collection Price'},
       ]
-  };
+  };*/
 
   //Our controller is what's going to connect our 'heavy lifting' itunesService with our view (index.html) so our user can see the results they get back from itunes.
 
@@ -65,16 +89,6 @@ app.controller('mainCtrl', function($scope, itunesService){
 
     //Code here
 
-
-    /*$scope.filterOptions = {
-        filterText: ''
-      };
-
-
-    $scope.gridOptions = {
-      data: 'myData',
-      filterOptions: $scope.filterOptions
-    };*/
 });
 
 
